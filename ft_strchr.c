ab1e65b0ae718b0fc31ddf4be214180eb3eeb674
tcharuel <tcharuel@student.42.fr>
2023-11-07 18:40:28 +0100

    Correct strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 10:23:26 by tcharuel          #+#    #+#             */
-/*   Updated: 2023/11/07 10:46:13 by tcharuel         ###   ########.fr       */
+/*   Updated: 2023/11/07 18:40:04 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	unsigned char	byte;
 
 	byte = (unsigned char)c;
-	while (*s != '\0')
-	{
-		if (*s == byte)
-			return ((char *)s);
+	while (*s != '\0' && *s != byte)
 		s++;
-	}
+	if (*s == byte)
+		return ((char *)s);
 	return (NULL);
 }
