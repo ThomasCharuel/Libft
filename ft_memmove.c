4635213cb75b70da5848043ef42bb382e533cadc
tcharuel <tcharuel@student.42.fr>
2023-11-07 14:53:23 +0100

    Correct memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:25:05 by tcharuel          #+#    #+#             */
-/*   Updated: 2023/11/06 19:14:37 by tcharuel         ###   ########.fr       */
+/*   Updated: 2023/11/07 14:53:04 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*buffer;
+	unsigned char	*buffer_src;
+	unsigned char	*buffer_dest;
+	size_t			i;
 
 	if (dest == src || n == 0)
 		return (dest);
-	buffer = (unsigned char *)malloc(n);
-	if (!buffer)
-		return (NULL);
-	ft_memcpy(buffer, src, n);
-	ft_memcpy(dest, buffer, n);
-	free(buffer);
+	buffer_src = (unsigned char *)src;
+	buffer_dest = (unsigned char *)dest;
+	i = n;
+	while (--i >= 0)
+		buffer_dest[i] = buffer_src[i];
 	return (dest);
 }
