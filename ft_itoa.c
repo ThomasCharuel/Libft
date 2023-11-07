6008e6de4ef5847c4f989653d6c185daff5e4924
tcharuel <tcharuel@student.42.fr>
2023-11-07 18:17:53 +0100

    Correct ft itoa

---
+++ ft_itoa.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 18:05:20 by tcharuel          #+#    #+#             */
+/*   Updated: 2023/11/07 18:10:01 by tcharuel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Allocates (with malloc(3)) and returns a string
+ * representing the integer received as an argument.
+ * Negative numbers must be handled.
+ *
+ * @param n The integer to convert.
+ *
+ * @return The string representing the integer.
+ * @retval NULL if the allocation fails.
+ */
+char	*ft_itoa(int n)
+{
+	char	*res;
+
+	(void)n;
+	res = NULL;
+	return (res);
+}
