64d0b87660bba2f2cd75daebe48b62219bc2bf16
tcharuel <tcharuel@student.42.fr>
2023-11-08 15:26:29 +0100

    lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:48:52 by tcharuel          #+#    #+#             */
-/*   Updated: 2023/11/08 13:56:55 by tcharuel         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:25:22 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@
  */
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	if (!lst || !*lst || !del)
+		return ;
 	if ((*lst)->next)
 		ft_lstclear(&(*lst)->next, del);
 	(*del)((*lst)->content);
